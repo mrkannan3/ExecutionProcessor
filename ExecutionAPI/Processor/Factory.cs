543bac8bc04dda78ef34909cb6e6847a24d6fa5f
Ramesh <ramesh.m.kannan@gmail.com>
2023-12-19 22:56:52 -0600

    Added code with no support of keyed service

--- ExecutionAPI/Processor/Factory.cs
+++ ExecutionAPI/Processor/Factory.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using System.Xml.Linq;
 
 namespace ExecutionAPI.Processor
 {
@@ -9,12 +10,53 @@ namespace ExecutionAPI.Processor
         {
             _serviceProvider = serviceProvider;
         }
-        public T GetRequiredService<T>(string? name = null)
+        /*public T GetRequiredService<T>(string? name = null)
         {
             if (!string.IsNullOrEmpty(name))
                 return _serviceProvider.GetKeyedService<T>(name);
             else
                 return _serviceProvider.GetService<T>();
+        }*/
+        public TService GetRequiredService<TService>(string name = null) where TService : class
+        {
+            if (name == null)
+            {
+                return _serviceProvider.GetService<TService>();
+            }
+            // Get the interface type
+            var interfaceType = typeof(TService);
+
+            // Determine the corresponding implementation type based on the request type
+            var implementationType = GetImplementationType(interfaceType, name);
+
+            // Resolve the service and cast it to the specified interface type
+            var service = _serviceProvider.GetServices<TService>();
+            foreach (var item in service)
+            {
+                if (item.GetType()  == implementationType)
+                {
+                    return item;
+                }
+            }
+
+            throw new InvalidOperationException($"Failed to resolve service for interface {interfaceType}.");
+            
         }
+
+        private Type GetImplementationType(Type interfaceType, string requestType)
+        {
+            var implementationTypeName = $"{requestType}{interfaceType.Name.Substring(1)}";
+
+            var implementationType = interfaceType.Assembly.GetTypes()
+                .FirstOrDefault(type => type.Name == implementationTypeName);
+
+            if (implementationType == null)
+            {
+                throw new NotSupportedException($"Unsupported request type: {requestType}");
+            }
+
+            return implementationType;
+        }
+
     }
 }
--- ExecutionAPI/Registry.cs
+++ ExecutionAPI/Registry.cs
@@ -15,12 +15,18 @@ namespace ExecutionAPI
         public static void AddExecutionDependnecies(this IServiceCollection services)
         {
             services.AddScoped<ExecutionProcessor>();
-            services.AddKeyedScoped<IOrderRetriever, EquityOrderRetriever>("Equity");
+            /*services.AddKeyedScoped<IOrderRetriever, EquityOrderRetriever>("Equity");
             services.AddKeyedScoped<IOrderRetriever, MFOrderRetriever>("MF");
             services.AddKeyedScoped<IProcessor, EquityProcessor>("Equity");
             services.AddKeyedScoped<IProcessor, MFProcessor>("MF");
             services.AddKeyedScoped<IPublisher, EventPublisher>("Event");
-            services.AddKeyedScoped<IPublisher, LogPublisher>("Log");
+            services.AddKeyedScoped<IPublisher, LogPublisher>("Log");*/
+            services.AddScoped<IOrderRetriever, EquityOrderRetriever>();
+            services.AddScoped<IOrderRetriever, MFOrderRetriever>();
+            services.AddScoped<IProcessor, EquityProcessor>();
+            services.AddScoped<IProcessor, MFProcessor>();
+            services.AddScoped<IPublisher, EventPublisher>();
+            services.AddScoped<IPublisher, LogPublisher>();
             //services.AddSingleton(Channel.CreateUnbounded<OrderRequest>());
             services.AddSingleton(Channel.CreateBounded<OrderRequest>(1));
             services.AddHostedService<ExecutionDispatcher>();
